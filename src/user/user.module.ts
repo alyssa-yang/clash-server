af01d457e78ae8ecfc9fcef9ceefea119909eadc
alyssa.yang <alyssa.yang@temu.com>
2023-08-04 01:07:33 +0800

    feature: format

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,23 +1,30 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './services/user.service';
-import { UserController } from './controllers/user.controller';
-import { SharedModule } from '../shared/shared.module';
-import { UserProviders } from './user.providers';
-import { RoleController } from './controllers/role.controller';
-import { RoleService } from './services/role.service';
-import { AuthService } from './services/auth.service';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import { AuthController } from './controllers/auth.controller';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { RedisModule } from '@nestjs-modules/ioredis';
+import { Module } from '@nestjs/common'
+import { UserService } from './services/user.service'
+import { UserController } from './controllers/user.controller'
+import { SharedModule } from '../shared/shared.module'
+import { UserProviders } from './user.providers'
+import { RoleController } from './controllers/role.controller'
+import { RoleService } from './services/role.service'
+import { AuthService } from './services/auth.service'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigService } from '@nestjs/config'
+import { AuthController } from './controllers/auth.controller'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { RedisModule } from '@nestjs-modules/ioredis'
 @Module({
   controllers: [UserController, RoleController, AuthController],
-  providers: [UserService, AuthService, ...UserProviders, RoleService, JwtStrategy],
+  providers: [
+    UserService,
+    AuthService,
+    ...UserProviders,
+    RoleService,
+    JwtStrategy
+  ],
   imports: [
     JwtModule.registerAsync({
       inject: [ConfigService],
-      imports: [SharedModule,
+      imports: [
+        SharedModule,
         RedisModule.forRootAsync({
           imports: [SharedModule],
           inject: [ConfigService],
@@ -26,8 +33,9 @@ import { RedisModule } from '@nestjs-modules/ioredis';
           })
         })
       ],
-      useFactory: (configService: ConfigService) => (configService.get('jwt'))
+      useFactory: (configService: ConfigService) => configService.get('jwt')
     }),
-    SharedModule]
+    SharedModule
+  ]
 })
-export class UserModule { }
+export class UserModule {}
