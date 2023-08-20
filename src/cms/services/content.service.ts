b4925bafb7ea509e4e0d87e5f686dcb4f6448328
alyssa.yang <alyssa.yang@temu.com>
2023-08-20 23:39:40 +0800

    puppeteer

--- src/cms/services/content.service.ts
+++ src/cms/services/content.service.ts
@@ -158,7 +158,7 @@ export class ContentService {
         '--font-antialiasing=standard',
         `--font-family="WenQuanYi Zen Hei"`
       ],
-      headless: true
+      headless: 'new'
     })
     const page = await browser.newPage()
 
